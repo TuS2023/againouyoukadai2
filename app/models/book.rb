00570c309b987888a9c03474c072173a7e0d0354
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-09-04 09:22:55 +0000

    search_fin

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,17 @@ class Book < ApplicationRecord
   def favorite?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
 end
