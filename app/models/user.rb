216da772224804c291950c8e82334ec3cfbb62d8
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-09-11 06:08:56 +0000

    DM&&FF

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :rooms, through: :entries
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 
   # フォローされている関連付け
